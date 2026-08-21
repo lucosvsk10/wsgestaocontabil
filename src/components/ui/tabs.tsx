16ef387e65783cc8b9c22d6ab6fa17b0b842376f
lucosvsk10 <123520265+lucosvsk10@users.noreply.github.com>
2026-08-20 21:43:19 -0300

    Atualiza src/components/ui/tabs.tsx

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -13,7 +13,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-[#f8f8f8] p-1 text-[#020817] dark:bg-navy-dark dark:text-gold shadow-sm",
+      "inline-flex h-10 items-center justify-center rounded-md bg-[#f8f8f8] p-1 text-[#020817] dark:bg-white/5 dark:text-white/70 shadow-sm",
       className
     )}
     {...props}
@@ -28,7 +28,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-[#020817] data-[state=active]:text-white dark:data-[state=active]:bg-gold dark:data-[state=active]:text-navy data-[state=active]:shadow-sm",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-[#020817] data-[state=active]:text-white dark:data-[state=active]:bg-white dark:data-[state=active]:text-[#020817] data-[state=active]:shadow-sm",
       className
     )}
     {...props}
