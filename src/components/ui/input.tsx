37f44d49deca5fd1a3d6d70544bb229ccaf55714
lucosvsk10 <123520265+lucosvsk10@users.noreply.github.com>
2026-08-20 21:43:14 -0300

    Atualiza src/components/ui/input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,8 +3,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -12,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-lg border border-[#e6e6e6] bg-white px-3 py-2 text-sm text-[#020817] shadow-sm transition-all duration-300 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#9ca3af] focus:border-[#efc349] focus:outline-none focus:ring-2 focus:ring-[#efc349]/10 disabled:cursor-not-allowed disabled:opacity-50 dark:border-[#efc349]/30 dark:bg-transparent dark:text-white dark:placeholder:text-white/50 dark:focus:border-[#efc349] dark:focus:ring-[#efc349]/10",
+          "flex h-10 w-full rounded-lg border border-[#e6e6e6] bg-white px-3 py-2 text-sm text-[#020817] shadow-sm transition-all duration-300 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-[#9ca3af] focus:border-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400/10 disabled:cursor-not-allowed disabled:opacity-50 dark:border-white/15 dark:bg-transparent dark:text-white dark:placeholder:text-white/50 dark:focus:border-white/35 dark:focus:ring-white/10",
           className
         )}
         ref={ref}
