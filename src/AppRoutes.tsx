da122b24fda0b2237ddffd78cf3f647972f6fa40
lucosvsk10 <123520265+lucosvsk10@users.noreply.github.com>
2026-08-21 13:37:51 -0300

    feat(engine): update src/AppRoutes.tsx

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -20,6 +20,7 @@ import { AdminLayout } from '@/components/admin/layout/AdminLayout';
 import AdminLancamentos from "./pages/AdminLancamentos";
 import AdminBalancete from "./pages/AdminBalancete";
 import AdminPlanoContas from "./pages/AdminPlanoContas";
+import AdminEngine from "./pages/AdminEngine";
 
 const AppRoutes = () => {
   const { userData, user } = useAuth();
@@ -131,6 +132,7 @@ const AppRoutes = () => {
       } />
       <Route path="/admin/lancamentos/balancete" element={<PrivateRoute requiredRole="admin"><AdminBalancete /></PrivateRoute>} />
       <Route path="/admin/lancamentos/plano-contas" element={<PrivateRoute requiredRole="admin"><AdminPlanoContas /></PrivateRoute>} />
+      <Route path="/admin/lancamentos/engine" element={<PrivateRoute requiredRole="admin"><AdminEngine /></PrivateRoute>} />
 
       {/* Client routes */}
       <Route path="/client/*" element={
