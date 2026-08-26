298e643077b18497e10d9c8e0c17da4d104025f0
lucosvsk10 <123520265+lucosvsk10@users.noreply.github.com>
2026-08-26 18:05:38 -0300

    fix(nfce): use official AL QR and key consultation URLs

--- supabase/functions/dfe-issue-native/qrcode.ts
+++ supabase/functions/dfe-issue-native/qrcode.ts
@@ -1,5 +1,5 @@
-const QR_CODE_URL_AL = "https://nfce.sefaz.al.gov.br/QRCode/consultarNFCe.jsp";
-const KEY_URL_AL_HOMOLOG = "https://nfce.sefaz.al.gov.br/consultaNFCeHomolog.htm";
+const QR_CODE_URL_AL = "http://nfce.sefaz.al.gov.br/QRCode/consultarNFCe.jsp";
+const KEY_URL_AL = "http://www.sefaz.al.gov.br/nfce/consulta";
 
 export function addNfceSupplement(xml: string, model: "55" | "65", chaveAcesso: string) {
   if (model !== "65") return xml;
@@ -8,7 +8,7 @@ export function addNfceSupplement(xml: string, model: "55" | "65", chaveAcesso: 
   // <url>?p=<chNFe>|3|<tpAmb>
   // Não utiliza CSC/idCSC neste fluxo.
   const qrCode = `${QR_CODE_URL_AL}?p=${chaveAcesso}|3|2`;
-  const supplement = `<infNFeSupl><qrCode><![CDATA[${qrCode}]]></qrCode><urlChave>${KEY_URL_AL_HOMOLOG}</urlChave></infNFeSupl>`;
+  const supplement = `<infNFeSupl><qrCode><![CDATA[${qrCode}]]></qrCode><urlChave>${KEY_URL_AL}</urlChave></infNFeSupl>`;
 
   // infNFeSupl é irmão de infNFe e deve ficar antes da Signature.
   if (xml.includes("<Signature")) return xml.replace("<Signature", `${supplement}<Signature`);
