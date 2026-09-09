5c45a075fd613fd6f6e026776886022d464289d2
lucosvsk10 <123520265+lucosvsk10@users.noreply.github.com>
2026-09-08 22:46:46 -0300

    fix: load Mercado Pago product typography last

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './styles/mercado-pago-product-font.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
