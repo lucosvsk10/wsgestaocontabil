9a56738e8090a90ebd5f5d452d2d804d3dc6af60
lucosvsk10 <123520265+lucosvsk10@users.noreply.github.com>
2026-09-11 23:17:19 -0300

    Improve fiscal emission workflows

---
+++ src/lib/saas/emissionValidation.ts
@@ -0,0 +1,13 @@
+export {
+  onlyDigits,
+  isValidCpf,
+  isValidCnpj,
+  isValidTaxId,
+  isValidAccessKey,
+  isPositiveAmount,
+  isDocumentNumber,
+  brazilStates,
+  parseAccessKeys,
+  validateMdfeKeys,
+} from '../../../supabase/functions/_shared/emissionValidation';
+
